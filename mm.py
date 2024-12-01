6771676e72522d29b58a989a6e0d7cc759b23b8a
vishwajeetsuhaspatil <bhagyashreesushv2@gmail.com>
2024-12-01 20:26:26 +0530

    bit-m

---
+++ mm.py
@@ -0,0 +1,10 @@
+def abc(n):
+    if(n | 1== n + 1):
+        return True
+    else:
+        return False
+a= int(input("enter a num"))
+if abc(a):
+    print("even num")
+else:
+    print("odd num")    
